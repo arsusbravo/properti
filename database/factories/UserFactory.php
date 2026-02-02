1082a2975dbdf02d5f70ca7b3276511892b7f16b
arsusbravo <ario_susmaryono@hotmail.com>
2026-02-02 19:15:54 +0100

    roll back to fake()

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => \Faker\Factory::create()->name(),
-            'email' => \Faker\Factory::create()->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
